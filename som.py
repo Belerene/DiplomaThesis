15af30320ee5a9c24330e56a3e87377bc405428e
Belerene <luk.ruck@gmail.com>
2017-03-27 14:16:44 +0200

    Update som.py

--- som.py
+++ som.py
@@ -7,18 +7,16 @@ Created on Wed Apr 20 16:38:23 2016
 
 import numpy as np
 import matplotlib.pyplot as plt
-#from mlp_toolkits.mplot3d import Axes3D
 
 class som:
     def __init__(self):
         data = self.loadTrain()
-        dim = 7
         self.data_num = 210
-      #  for i in range(dim-1):
-      #      data[:,i] = (data[:,i]/ (np.max(data[:,i])-np.min(data[:,i])))
         self.w = 20
         self.h = 15        
-        W = np.random.random_sample((dim,self.w,self.h))
+        W = np.random.random_sample((self.w,self.h))
+        Ai = np.random.random_sample((self.w,self.h))
+        bias = np.random.random_sample((self.w,self.h))
         eps = 100
         alpha_s = 1
         alpha_f = 0.5
@@ -27,13 +25,11 @@ class som:
         lambdaOld = lambda_s
         alphaOld = alpha_s
         estQEs = np.ones([1,1])
-        adjustment = np.ones([1,1])
         alphas = np.ones([1,1])
         lambdas = np.ones([1,1])
         alpha = alpha_s
         for ep in range(eps):
             print(str(ep))
-            adjust = 0.0
             QE = 0.0
             lamb = lambda_s*np.exp(-float(ep)/float(eps))
             #alpha = alpha_s*np.exp(-float(ep)/float(eps))
@@ -53,31 +49,12 @@ class som:
                 QE = QE+dist
                 for width in range(self.w):
                     for height in range(self.h):
-                        W[:,width,height] = W[:,width,height] + alpha * self.neighFunc(width,height,bestCoords[0],bestCoords[1],lamb) * (x-W[:,width,height])                        
-            adjust = self.averageAdjust(wOldCoords,W)
+                        W[width,height] = W[width,height] + alpha * self.neighFunc(width,height,bestCoords[0],bestCoords[1],lamb) * (x-W[width,height])                        
             QE = QE/float(self.data_num)
-            adjustment = np.append(adjustment,adjust)
             estQEs = np.append(estQEs, QE)
         estQEs = np.delete(estQEs,0,0)
-        adjustment = np.delete(adjustment,0,0)
         alphas = np.delete(alphas,0,0)
         lambdas = np.delete(lambdas,0,0)
-        self.plotclass(data[0:7,:],data[7,:],W)
-        print(adjustment) 
-        plt.style.use('ggplot')
-        plt.figure()
-        plt.hold(True)
-        plt.plot(adjustment)
-        plt.figure()
-        plt.plot(estQEs)
-        print(str(estQEs))
-        plt.figure()
-        plt.plot(alphas)
-        plt.figure()
-        plt.plot(lambdas)
-        plt.hold(False)
-        self.plotHeatMapsForCoord(W)
-        self.plotUMap(W)
         
             
     def averageAdjust(self,wOld, wNew):
@@ -87,87 +64,46 @@ class som:
                 dist += np.linalg.norm(wNew[:,w,h] - wOld[:,w,h],2)
         return dist/float(self.w*self.h)
                 
-        
-    def plotclass(self,data,cls,w):
-        plt.figure()
-        plt.hold(True)
-        plt.axis([-0.5,self.w + 0.1,-0.5,self.h+0.1])
-        for i in range(self.data_num):
-            x = data[:,i]
-            coorCN = self.findClosestNeuron(x, w)
-            coorCN = coorCN[0]
-            if cls[i] == 1:
-                plt.plot(coorCN[0], coorCN[1], marker ='o', mec = 'lime', mew='2', mfc = 'None', ms = 12.5, label='Trieda 1')
-            elif cls[i] == 2:
-                plt.plot(coorCN[0], coorCN[1], marker ='x', mec = 'orange', mew='2', ms = 8)
-            else:
-                plt.plot(coorCN[0], coorCN[1], marker ='v', mec = 'w', mew='2', mfc = 'navy', ms = 12.5)
-        plt.grid(True)
-        plt.hold(False)
-        plt.show()
-        
-    
-    def plotDistanceHeatMap(self, w):
-        distMapVert = np.zeros((self.w,self.h))
-        for i in range(self.w):
-            for j in range(self.h):                
-                if j+1< self.h:
-                    distMapVert[i,j] = self.distance(w[:,i,j],w[:,i,j+1])
-                    if j > 0:
-                        distMapVert[i,j] = (distMapVert[i,j]+self.distance(w[:,i,j],w[:,i,j-1]))/float(1)
-                else:
-                    distMapVert[i,j] = self.distance(w[:,i,j],w[:,i,j-1])
-        distMapHor = np.zeros((self.w,self.h))
-        for i in range(self.w):
-            for j in range(self.h):
-                if i+1< self.w:
-                    distMapHor[i,j] = self.distance(w[:,i,j],w[:,i+1,j])
-                    if i > 0:
-                        distMapHor[i,j] = (distMapHor[i,j]+self.distance(w[:,i,j],w[:,i-1,j]))/float(1)
-                else:
-                    distMapHor[i,j] = self.distance(w[:,i,j],w[:,i-1,j])
-        plt.figure()
-        plt.style.use('grayscale')        
-        plt.imshow(distMapVert.T)        
-        plt.figure()
-        plt.imshow(distMapHor.T)
-        
-    def plotUMap(self, w):
-        distUMap = np.zeros((self.w,self.h))
-        for i in range(self.w):
-            for j in range(self.h):                
-                if j+1< self.h:
-                    distUMap[i,j] = self.distance(w[:,i,j],w[:,i,j+1])
-                    if j > 0:
-                        distUMap[i,j] = (distUMap[i,j]+self.distance(w[:,i,j],w[:,i,j-1]))/float(1)
-                else:
-                   distUMap[i,j] = self.distance(w[:,i,j],w[:,i,j-1])
-                if i+1< self.w:
-                    distUMap[i,j] = distUMap[i,j]+self.distance(w[:,i,j],w[:,i+1,j])
-                    if i > 0:
-                        distUMap[i,j] = (distUMap[i,j]+self.distance(w[:,i,j],w[:,i-1,j]))/float(1)
-                else:
-                    distUMap[i,j] = distUMap[i,j]+self.distance(w[:,i,j],w[:,i-1,j])
-                
-        plt.figure()
-        plt.style.use('grayscale')        
-        plt.imshow(distUMap.T,interpolation='none')
-        
-    def plotHeatMapsForCoord(self,w):
-        for k in range(7):
-            plt.figure()
-            plt.imshow(w[k,:,:].T)
-            
-                
-        
-            
+
+    """
+    Returns activity of a single neuron - formula (4)
+    """        
     def activity(self,x,w,sensitivity,bias):
         return bias*np.exp(-sensitivity*self.euclidDist(x,w)**2)
+     
+     
+    """
+    Returns activies for every neuron - formula (5)
+    """ 
+    def somActivity(self, x, W, sensitivity, bias):
+        aj = 0
+        for width in range(self.w):
+            for height in range(self.h):
+                aj = aj+self.activity(x,W[width,height],sensitivity,bias[width,height])    
+        Ai = np.random.random_sample((self.w,self.h))
+        for width in range(self.w):
+            for height in range(self.h):
+                Ai[width,height] = self.activity(x,W[width,height],sensitivity,bias[width,height])/aj
+        return Ai
+     
+     
+     
+    """
+    Returns block-wise reconstructed input - formula (7) 
+    
+    It should be implemented as Wjk (weight corresponding to a block), but is implemented as Wj
+    """
+    def reconstructInput(self,Ai,W,t):
+        y = np.random.random_sample((len(t)))
+        for k in range(t):
+            summed = 0
+            for width in range(self.w):
+                for height in range(self.h):
+                    summed = Ai[width,height] * W[width,height]  
+            y[k] = t[k] * summed
+        return y
         
-    def somActivity(self, ai, aj, bias, sensitivity):
-        return self.activity(ai)/np.sum(self.activity(aj))
-     
-     
+        
     def dist(self,x,w,b):
         return np.sum(b * self.euclidDist(x,w))
         
@@ -193,15 +129,5 @@ class som:
                     distance = d
                     coords = [nx,ny]
         return [coords,distance]
-        
-    
-    
-    def loadTrain(self):
-        trainn = np.loadtxt('seeds.txt', dtype={'names': ('1', '2', '3', '4', '5', '6', '7', '8'),'formats': ('float64', 'float64', 'float64','float64','float64','float64','float64','float64')})
-        train = np.zeros([210,8])
-        for i in range(0,210):
-            for j in range(0,8):
-                train[i][j] = trainn[i][j]
-        return train.T
 
 som()
